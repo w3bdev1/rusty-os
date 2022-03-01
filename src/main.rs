7aa88cbff801fa4df05c654417ef411483b680e0
w3bdev <w3bdev@protonmail.com>
2022-03-01 21:28:27 +0530

    Notes on text mode memory

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@
 use core::panic::PanicInfo;
 
 static HELLO: &[u8] = b"Hello World!";
+// 'HeLlo'
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -12,6 +13,15 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    // The text screen video memory for colour monitors resides at 0xB8000
+    //The VGA text buffer is accessible via memory-mapped I/O to the address 0xb8000. This means that reads and writes to that address don’t access the RAM, but directly the text buffer on the VGA hardware. This means that we can read and write it through normal memory operations to that address.
+    // Text mode memory takes two bytes for every "character" on screen. One is the ASCII code byte, the other the attribute byte.
+    // Bit(s)    Value
+    // 0-7	     ASCII code point
+    // 8-11	     Foreground color
+    // 12-14	 Background color
+    // 15	     Blink
+
     let vga_buffer = 0xb8000 as *mut u8;
 
     for (i, &byte) in HELLO.iter().enumerate() {
